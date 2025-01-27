52a6196abb030377f674e9673a1fd58d5bec5dfd
EthanB2004 <bradshawre341@mail.sandhills.edu>
2025-01-27 10:25:15 -0500

    Update HelloWorld.java
    added code

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,1 +1,11 @@
-Whats Good Gang
+
+///author: Ethan Bradshaw
+//date: Jan 27th 2024
+// First Java Programming - Prints Hello World
+import java.util.Scanner;
+
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
